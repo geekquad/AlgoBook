153eacb2650ae51e685c6efe2fe6e1c58499d28c
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-09-29 11:37:12 +0530

    Create heapSort.py

---
+++ python/Sorting Algorithms/heapSort.py
@@ -0,0 +1,38 @@
+def heapify(unsorted, index, heap_size):
+    largest = index
+    left_index = 2 * index + 1
+    right_index = 2 * index + 2
+    if left_index < heap_size and unsorted[left_index] > unsorted[largest]:
+        largest = left_index
+
+    if right_index < heap_size and unsorted[right_index] > unsorted[largest]:
+        largest = right_index
+
+    if largest != index:
+        unsorted[largest], unsorted[index] = unsorted[index], unsorted[largest]
+        heapify(unsorted, largest, heap_size)
+
+
+def heapSort(unsorted):
+    """
+    Examples:
+    >>> heapSort([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+    >>> heapSort([])
+    []
+    >>> heapSort([-2, -5, -45])
+    [-45, -5, -2]
+    """
+    n = len(unsorted)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(unsorted, i, n)
+    for i in range(n - 1, 0, -1):
+        unsorted[0], unsorted[i] = unsorted[i], unsorted[0]
+        heapify(unsorted, 0, i)
+    return unsorted
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(heapSort(unsorted))
