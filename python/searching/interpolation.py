dd41662c0cddde11cdc223fba30b1505daa38106
Rohit_Joshi <rohitjoshi632001@gmail.com>
2020-10-01 21:49:37 +0530

    Added comments explaining  variable names

--- python/searching/interpolation.py
+++ python/searching/interpolation.py
@@ -3,6 +3,15 @@
 #following is the code of interpolation search:
 
 # Python program to implement interpolation search 
+#Variable naming:
+"""
+1) lys - our input array
+2) val - the element we are searching for
+2) index - the probable index of the search element. This is computed to be a higher value when val
+is closer in value to the element at the end of the array (lys[high]), and lower when val
+is closer in value to the element at the start of the array (lys[low])
+4) low - the starting index of the array
+5) high - the last index of the array"""
   
 def InterpolationSearch(lys, val):
     low = 0
