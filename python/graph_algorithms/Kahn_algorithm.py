10e7be4eab5e9f9cf401c84d40456ec04e1536b0
ishanvj2207 <ishan.vijay2000@gmail.com>
2020-10-01 19:47:58 +0530

    Added Kahn Algorithm

---
+++ python/graph_algorithms/Kahn_algorithm.py
@@ -0,0 +1,69 @@
+# Kahn Algorithm for topological sorting of a graph
+# Adjacency lists representation
+
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.graph = defaultdict(list)  # dictionary containing adjacency List
+        self.V = vertices  # No. of vertices
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def topologicalSort(self):
+
+        # vector to store indegrees of all vertices. Initialize all indegrees as 0.
+        in_degree = [0]*(self.V)
+
+        # Traverse adjacency lists to fill indegrees of vertices.
+        for i in self.graph:
+            for j in self.graph[i]:
+                in_degree[j] += 1
+
+        # Create an queue and enqueue all vertices with indegree 0
+        queue = []
+        for i in range(self.V):
+            if in_degree[i] == 0:
+                queue.append(i)
+
+        # Initialize count of visited vertices
+        cnt = 0
+
+        # Create a vector to store topological ordering of the vertices
+        top_order = []
+
+        # One by one dequeue vertices from queue and enqueue
+        # adjacents if indegree of adjacent becomes 0
+        while queue:
+
+            u = queue.pop(0)
+            top_order.append(u)
+
+            for i in self.graph[u]:
+                in_degree[i] -= 1
+
+                if in_degree[i] == 0:
+                    queue.append(i)
+
+            cnt += 1
+
+        # Check if there was a cycle
+        if cnt != self.V:
+            print("There exists a cycle in the graph")
+        else:
+            # Print topological order
+            print(top_order)
+
+
+g = Graph(6)
+g.addEdge(5, 2)
+g.addEdge(5, 0)
+g.addEdge(4, 0)
+g.addEdge(4, 1)
+g.addEdge(2, 3)
+g.addEdge(3, 1)
+
+print("Following is a Topological Sort of the given graph")
+g.topologicalSort()
