a70c76f6dc41245f5306d9e3c8f147ed173e02da
Sarthak Regmi <sarthakregmi18@gmail.com>
2020-10-04 22:11:52 +0545

    Updated Double sort

--- python/sorting/Doublesort.py
+++ python/sorting/Doublesort.py
@@ -1,27 +1,22 @@
-# Takes the user's input and removes the spaces
-raw_input = str(input("Enter input for double sorting: "))
-stri = list(raw_input.replace(' ',''))
-
-# Creates a list called counts, where I will store counts for each letter of my string
-# I chose to use a list over a dictionary since dictionaries don't retain order
-
-counts = []
-
-# I loop through each letter that exists in the string, but I want to be careful to
-# use a set here so that my letters are unique and I'm not revisiting the same letter
-# twice. Python sets are magical and amazing! I add to my counts list a tuple of:
-# the number of letters in the string, and the letter itself like: (8: 'o')
-
-for letter in set(stri):
-    counts.append((stri.count(letter), letter))
-
-# I now have a list, not sorted in any way. I can both sort by both the frequency and
-# the letter at the same time.
-
-double_sort = sorted(counts, key=lambda tup: (-tup[0], tup[1]))
-
-# Now I've got a sorted list of tuples, and I can print out the second item (letter) of
-# each tuple in order of frequency and then alphabetical
-
-for i in range(0, len(double_sort)):
-    print (double_sort[i][1])
+def double_sort(lst):
+    no_of_elements = len(lst)
+    for i in range(0, int(((no_of_elements-1)/2) + 1)):
+        # we don't need to traverse to end of list as
+        for j in range(0, no_of_elements - 1):
+            if (lst[j + 1] < lst[j]):
+                # applying bubble sort algorithm from left to right (or forwards)
+                temp = lst[j + 1]
+                lst[j + 1] = lst[j]
+                lst[j] = temp
+            if (lst[no_of_elements - 1 - j] < lst[no_of_elements - 2 - j]):
+                # applying bubble sort algorithm from right to left (or backwards)
+                temp = lst[no_of_elements - 1 - j]
+                lst[no_of_elements - 1 - j] = lst[no_of_elements - 2 - j]
+                lst[no_of_elements - 2 - j] = temp
+        return lst
+if __name__ == "__main__":
+    print("enter the list to be sorted")
+    lst = [int(x) for x in input().split()]  # inputing elements of the list in one line
+    sorted_lst = double_sort(lst)
+    print("the sorted list is")
+    print(sorted_lst)
