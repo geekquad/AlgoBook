77e4e8fbf204d5520d8ce146506dcd24efe6bee7
Manthan Bhikadiya <61309101+manthan89-py@users.noreply.github.com>
2020-10-05 23:55:09 +0530

    Subset Sum - Dynamic programming

--- python/dynamic_programming/Subset_Sum_dynamic_programming.py
+++ python/dynamic_programming/Subset_Sum_dynamic_programming.py
@@ -1,4 +1,7 @@
-# problem says given array has given sum then true or not for false.
+# PROBLEM STATEMENT:
+# Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+# Example [ 1 ,5 , 11, 5] Answer True Explanation : [1,5,5] and [11] are two patitioned with equal sum that's why its said true
+# we are simply using dynamic programming and check if include that element and if not include then what happend.
 
 def subset_sum(arr , sum):
     dp = [[False for x in range(sum+1)] for x in range(len(arr) + 1)]
