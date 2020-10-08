558bc18f0d396c6019b63645ea7d2b07c7767bfc
Pradeep <58101085+Iampradeepkundra@users.noreply.github.com>
2020-10-08 19:18:18 +0530

    Update Cocktail Sort.cpp
    i have changed the entire code this is now user defined
    thank you

--- cpp/sorting/Cocktail Sort.cpp
+++ cpp/sorting/Cocktail Sort.cpp
@@ -1,30 +1,38 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+void swap(int *a, int *b) {
+   int temp;
+   temp = *a;
+   *a = *b;
+   *b = temp;
+}
+void ShakerSort(int a[], int m) {
+   int i, j, k;
+   for(i = 0; i < m;) {
+      for(j = i+1; j < m; j++) {
+         if(a[j] < a[j-1])
+            swap(&a[j], &a[j-1]);
+      }
+      m--;
+      for(k = m-1; k > i; k--) {
+         if(a[k] < a[k-1])
+            swap(&a[k], &a[k-1]);
+      }
+      i++;
+   }
+}
 int main() {
-   int arr[] = { 5, 3, 4, 2, 1 };
-   int m=5;
-   int n, c;
-   n=m;
-   do {
-      for (int i = 0; i < n - 1; i++) {
-         if (arr[i] > arr[i + 1]) {
-            arr[i] = arr[i] + arr[i + 1];
-            arr[i + 1] = arr[i] - arr[i + 1];
-            arr[i] = arr[i] - arr[i + 1];
-         }
-      }
-      n = n - 1;
-      for (int i=m-1, c = 0; i >= c; i--) {
-         if(arr[i] < arr[i - 1]) {
-            arr[i] = arr[i] + arr[i - 1];
-            arr[i - 1] = arr[i] - arr[i - 1];
-            arr[i] = arr[i] - arr[i - 1];
-         }
-      }
-      c = c + 1;
+   int n, i;
+   cout<<"\nEnter the number of data element to be sorted: ";
+   cin>>n;
+   int a[n];
+   for(i = 0; i < n; i++) {
+      cout<<"Enter element "<<i+1<<": ";
+      cin>>a[i];
    }
-   while (n != 0 && c != 0);
-   for (int i = 0; i < m; i++) {
-      cout<< arr[i]<<"\t";
-   }
+   ShakerSort(a, n);
+   cout<<"\nSorted Data ";
+   for (i = 0; i < n; i++)
+      cout<<"->"<<a[i];
+   return 0;
 }
