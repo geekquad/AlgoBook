b04d88dd5d850263b5867863786c8d83cb7748e0
Vrok2401 <vftan241@gmail.com>
2020-10-08 14:09:12 +0800

    ExtEuclid

---
+++ python/maths/Extended Euclidean algorithm/ExtEuclid.py
@@ -0,0 +1,16 @@
+def ext_euclid(a, b):
+    x,y, u,v = 0,1, 1,0
+
+    while a != 0:
+        #quotient (q), remainder (r)
+        q, r = b // a, b % a
+        m, n = x - u*q, y - v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+        
+        print("Quotient: "+str(q))
+        print("GCD: "+str(b))
+        print("Remainder: "+str(a))
+        print(x,y,u,v)
+
+    gcd = b
+    return gcd,x,y
