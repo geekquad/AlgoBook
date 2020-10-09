103442c0251648eff9ff1bbb0b99c466b2a30ee2
Aditya Kumar Gupta <adityaastranaut@gmail.com>
2020-10-09 09:52:41 +0530

    Update relu.py

--- ml/Perceptrons/relu.py
+++ ml/Perceptrons/relu.py
@@ -3,4 +3,18 @@
 import numpy as np
 
 def relu(z):
+    """
+    Implements the relu function
+
+    Parameters:
+        vector (np.array,list,tuple): A  numpy array of shape (1,n)
+        consisting of real values or a similar list,tuple
+
+
+    Returns:
+        relu_vec (np.array): The input numpy array, after applying
+        relu.
+    """
+    
+    # compare two arrays and then return element-wise maxima
     return np.maximum(0,z) 
