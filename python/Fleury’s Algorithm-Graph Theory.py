77e38ed3061a994790e6727998a11e6b013e7329
Dasith U Edirisinghe <58045206+DasithEdirisinghe@users.noreply.github.com>
2020-10-19 07:47:13 +0530

    Update Fleury’s Algorithm-Graph Theory.py

--- python/Fleury’s Algorithm-Graph Theory.py
+++ python/Fleury’s Algorithm-Graph Theory.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
   
-  class Graph:
-  
+class Graph:
+
     def __init__(self,vertices):
         self.V= vertices 
         self.graph = defaultdict(list) 
         self.Time = 0
-  
+
     def addEdge(self,u,v):
         self.graph[u].append(v)
         self.graph[v].append(u)
- 
+
     def rmvEdge(self, u, v):
         for index, key in enumerate(self.graph[u]):
             if key == v:
@@ -18,7 +18,7 @@ from collections import defaultdict
         for index, key in enumerate(self.graph[v]):
             if key == u:
                 self.graph[v].pop(index)
- 
+
     def DFSCount(self, v, visited):
         count = 1
         visited[v] = True
@@ -26,7 +26,7 @@ from collections import defaultdict
             if visited[i] == False:
                 count = count + self.DFSCount(i, visited)        
         return count
- 
+
     def isValidNextEdge(self, u, v):
        
         if len(self.graph[u]) == 1:
@@ -34,25 +34,25 @@ from collections import defaultdict
         else:
             visited =[False]*(self.V)
             count1 = self.DFSCount(u, visited)
- 
+
             self.rmvEdge(u, v)
             visited =[False]*(self.V)
             count2 = self.DFSCount(u, visited)
- 
+
             self.addEdge(u,v)
- 
+
             return False if count1 > count2 else True
- 
- 
+
+
     def printEulerUtil(self, u):
         for v in self.graph[u]:
             if self.isValidNextEdge(u, v):
                 print("%d-%d " %(u,v)),
                 self.rmvEdge(u, v)
                 self.printEulerUtil(v)
- 
- 
-   def printEulerTour(self):
+
+
+    def printEulerTour(self):
         u = 0
         for i in range(self.V):
             if len(self.graph[i]) %2 != 0 :
