e8052d72a35230aabb1616a8b11d9c0894129167
Nithesh Gurudaas <nithesh.gurudaas1103@gmail.com>
2020-10-28 12:57:02 +0530

    Created new directory web application and added a project Insurance website to it

---
+++ Web Application/Insurance website/Readme.md
@@ -0,0 +1,13 @@
+Insurance website by Nithesh Gurudaas
+
+Languages used: HTML, CSS, JavaScript
+
+How to use:
+1. Open index.html. Content provided is only for show. Slideshow can be changed by clicking on the relevant checkbox. Click on apply now to be redirected to reg.html
+2. reg.html is a simple form that needs to be filled up. Form validation is done using JavaScript. The rules applied in form validation is as follows:
+    a. User id must be between length 5 and 12
+    b. Password must be between 7-12 characters
+    c. Name must contain letters only, and address can contain alpha numeric values
+    d. Mobile number should be of length 10 characters, and must contain numbers only
+    e. Email ID should be a valid one, and zip code should be all numeric
+   If all the criteria are met, you will get a succesful submission message, else you would get a relevant error message
---
+++ Web Application/Insurance website/form.js
@@ -0,0 +1,176 @@
+function formValidation()
+{
+var uid = document.registration.userid;
+var passid = document.registration.passid;
+var uname = document.registration.username;
+var uadd = document.registration.address;
+var ucountry = document.registration.country;
+var uzip = document.registration.zip;
+var uemail = document.registration.email;
+var umsex = document.registration.msex;
+var ufsex = document.registration.fsex;
+var mob=document.registration.numb;
+    if(userid_validation(uid,5,12))
+{
+if(passid_validation(passid,7,12))
+{
+if(allLetter(uname))
+{
+if(alphanumeric(uadd))
+{
+if(mobilenum(mob))
+{
+if(countryselect(ucountry))
+{
+if(allnumeric(uzip))
+{
+if(ValidateEmail(uemail))
+{
+if(validsex(umsex,ufsex))
+{
+}
+}
+} 
+}
+} 
+}
+}
+}
+}
+return false;
+
+} function userid_validation(uid,mx,my)
+{
+var uid_len = uid.value.length;
+if (uid_len == 0 || uid_len >= my || uid_len < mx)
+{
+alert("User Id should not be empty / length be between "+mx+" to "+my);
+uid.focus();
+return false;
+}
+return true;
+}
+function passid_validation(passid,mx,my)
+{
+var passid_len = passid.value.length;
+if (passid_len == 0 ||passid_len >= my || passid_len < mx)
+{
+alert("Password should not be empty / length be between "+mx+" to "+my);
+passid.focus();
+return false;
+}
+return true;
+}
+function allLetter(uname)
+{ 
+var letters = /^[A-Za-z]+$/;
+if(uname.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('Username must have alphabet characters only');
+uname.focus();
+return false;
+}
+}
+function alphanumeric(uadd)
+{ 
+var letters = /^[0-9a-zA-Z]+$/;
+if(uadd.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('User address must have alphanumeric characters only');
+uadd.focus();
+return false;
+}
+}
+function countryselect(ucountry)
+{
+if(ucountry.value == "Default")
+{
+alert('Select your country from the list');
+ucountry.focus();
+return false;
+}
+else
+{
+return true;
+}
+}
+function allnumeric(uzip)
+{ 
+var numbers = /^[0-9]+$/;
+if(uzip.value.match(numbers))
+{
+return true;
+}
+else
+{
+alert('ZIP code must have numeric characters only');
+uzip.focus();
+return false;
+}
+}
+function mobilenum(mob)
+{ 
+var numbers = /^[0-9]+$/;
+if(mob.value.match(numbers))
+{
+if(mob.value.length==10)
+return true;
+else
+{
+alert('Illegal number length');
+mob.focus();
+return false;
+}
+}
+else
+{
+alert('ZIP code must have numeric characters only');
+uzip.focus();
+return false;
+}
+}
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+} function validsex(umsex,ufsex)
+{
+x=0;
+
+if(umsex.checked) 
+{
+x++;
+} if(ufsex.checked)
+{
+x++; 
+}
+if(x==0)
+{
+alert('Select Male/Female');
+umsex.focus();
+return false;
+}
+else
+{
+alert('Form Succesfully Submitted');
+window.location.reload()
+return true;
+}
+}
