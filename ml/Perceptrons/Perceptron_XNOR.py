4a141bee3365caa1e62b4fe615f276600d513315
Aman Singh <42384948+AmanSingh0-0@users.noreply.github.com>
2020-12-07 06:37:14 +0530

    Create Perceptron_XNOR.py

---
+++ ml/Perceptrons/Perceptron_XNOR.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+def step_function(value):
+  if value >= 0:
+    return 1
+  else:
+    return 0
+  
+def perceptron(x, weight, bias):
+  v = np.dot(weight, x) + bias
+  y = step_function(v)
+  
+  return y
+def AND_perceptron(x):
+  w = np.array([1,1])
+  b = -1.5
+  return perceptron(x, w, b)
+
+def OR_perceptron(x):
+  w = np.array([1,1])
+  b = -0.5
+  return perceptron(x, w, b)
+  
+def NOT_perceptron(x):
+  return perceptron(x, weight=-1, bias=0.5)
+
+print('NOT(1) = ', NOT_perceptron(1))
+print('NOT(0) = ', NOT_perceptron(0))
+
+input_binarylogics = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+def XOR_perceptron(x):
+    gate_1 = AND_perceptron(x)
+    gate_2 = NOT_perceptron(gate_1)
+    gate_3 = OR_perceptron(x)
+    new_x = np.array([gate_2, gate_3])
+    output = AND_perceptron(new_x)
+    
+    return output
+  
+def XNOR_perceptron(x):
+  gate_1 = XOR_perceptron(x)
+  output = NOT_perceptron(gate_1)
+  
+  return output
+  
+output_XNOR = []
+  
+
+for input in input_binarylogics:
+  result = XNOR_perceptron(input)
+  output_XNOR.append(result)
+        
+
+output_binarylogics_XNOR = np.array(output_XNOR)
+
+XNOR_table = pd.DataFrame({'Input 1':input_binarylogics[:,0], 
+                          'Input 2':input_binarylogics[:,1],
+                          'Output':output_binarylogics_XNOR})
+
+#XNOR_table (print this)
