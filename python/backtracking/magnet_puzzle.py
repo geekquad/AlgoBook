fc69e8f6a22b229affcb0cb93dce6be02c14f1db
Sounak Mandal <sounakmandal21@gmail.com>
2020-12-08 23:40:46 +0530

    problem formalisation

---
+++ python/backtracking/magnet_puzzle.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+ 
+    # indicates the count of + or - at the top (+), bottom (-), left (+) and right (-) edges respectively.
+    # value of -1 indicates any number of + or - signs
+    top = [1, -1, -1, 2, 1, -1]
+    bottom = [2, -1, -1, 2, -1, 3]
+    left = [2, 3, -1, -1, -1]
+    right = [-1, -1, -1, 1, -1]
+ 
+    # rules matrix
+    str = [
+        ['L', 'R', 'L', 'R', 'T', 'T'],
+        ['L', 'R', 'L', 'R', 'B', 'B'],
+        ['T', 'T', 'T', 'T', 'L', 'R'],
+        ['B', 'B', 'B', 'B', 'T', 'T'],
+        ['L', 'R', 'L', 'R', 'B', 'B']
+    ]
+ 
+    (M, N) = (5, 6)
