0f8c00ffaf22ef200e8b4b62b4091584497143da
verma-shivani <60282597+verma-shivani@users.noreply.github.com>
2021-01-06 01:39:57 +0530

    Update SuffixArray.cpp
    made the specified changes.Updated SuffixArray.cpp

--- cpp/Strings/SuffixArray.cpp
+++ cpp/Strings/SuffixArray.cpp
@@ -1,6 +1,8 @@
+
 #include <iostream> 
-#include <cstring> 
+#include <string> 
 #include <algorithm> 
+#include<bits/stdc++.h>
 using namespace std; 
 
 struct suffix 
@@ -8,16 +10,16 @@ struct suffix
 	int index; 
 	char *suff; 
 }; 
- 
+
 int cmp(struct suffix a, struct suffix b) 
 { 
-	return strcmp(a.suff, b.suff) < 0? 1 : 0; 
+	return strcmp(a.suff, b.suff)<0?1:0; 
 } 
 
 int *SuffixArray(char *txt, int n) 
 {  
 	struct suffix suffixes[n]; 
- 
+
 	for (int i = 0; i < n; i++) 
 	{ 
 		suffixes[i].index = i; 
@@ -29,10 +31,10 @@ int *SuffixArray(char *txt, int n)
 	int *suffixArr = new int[n]; 
 	for (int i = 0; i < n; i++) 
 		suffixArr[i] = suffixes[i].index; 
- 
+
 	return suffixArr; 
 } 
- 
+
 void printArr(int arr[], int n) 
 { 
 	for(int i = 0; i < n; i++) 
@@ -42,27 +44,27 @@ void printArr(int arr[], int n)
 
 int main() 
 { 
-	string str;
-	cin>>str;
-	
 	//char txt[] = "banana"; 
-	
+
 	/*
 	Let the given string be "banana".
-
 	0 banana                          5 a
 	1 anana     Sort the Suffixes     3 ana
 	2 nana      ---------------->     1 anana  
 	3 ana        alphabetically       0 banana  
 	4 na                              4 na   
 	5 a                               2 nana
-
 	So the suffix array for "banana" is {5, 3, 1, 0, 4, 2}
 	*/
 	
+	char str[100000];
+	cin>>str;
+
 	int n = strlen(str); 
-	int *suffixArr =SuffixArray(str, n); 
+
+	int *suffixArr=SuffixArray(str,n); 
 	cout << "Following is suffix array for " << str << endl; 
-	printArr(suffixArr, n); 
+	printArr(suffixArr,n); 
 	return 0; 
 } 
+
